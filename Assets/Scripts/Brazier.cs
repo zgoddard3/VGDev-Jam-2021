f96691b8f6fc804a65ad6d85af4177083041c6d2
zgoddard3 <zgoddard3@gatech.edu>
2021-07-10 15:15:09 -0400

    New brazier sprite

--- Assets/Scripts/Brazier.cs
+++ Assets/Scripts/Brazier.cs
@@ -6,16 +6,20 @@ using UnityEngine.SceneManagement;
 public class Brazier : MonoBehaviour
 {
     private Canvas canvas;
+    private Transform player;
     // Start is called before the first frame update
     void Start()
     {
         canvas = transform.Find("Canvas").GetComponent<Canvas>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.E) && (player.position - transform.position).magnitude < 5f) {
+
+        }
     }
 
     private IEnumerator ReturnToMenu() {
