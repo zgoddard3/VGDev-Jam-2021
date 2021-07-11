ca999ec156765539b945c3c465323d2a0601ab6a
zgoddard3 <zgoddard3@gatech.edu>
2021-07-11 13:34:52 -0400

    Options menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,9 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+    public AudioMixerGroup sfx;
+    public AudioMixerGroup music;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,10 +20,26 @@ public class MainMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            CloseOptions();
+        }
     }
 
     public void StartGame() {
         SceneManager.LoadScene("Level 1");
+    }
+
+    public void OpenOptions() {
+        mainMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void CloseOptions() {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
+
+    public void SetSFXVolume() {
+        
     }
 }
